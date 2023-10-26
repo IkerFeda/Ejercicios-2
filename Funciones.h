cb2f4fe87a5aa33595bc8c05cd29212cdcb4e015
Francisco <146821904+fferrlop@users.noreply.github.com>
2023-10-26 09:48:30 +0200

    Add files via upload

---
+++ Funciones.h
@@ -0,0 +1,17 @@
+//
+// Created by Francisco on 25/10/2023.
+//
+
+#ifndef INC_6_PUNTEROS_FUNCIONES_H
+#define INC_6_PUNTEROS_FUNCIONES_H
+
+
+int suma(int a, int b);
+
+int resta(int a, int b);
+
+int multiplicacion (int a, int b);
+
+#endif
+
+
